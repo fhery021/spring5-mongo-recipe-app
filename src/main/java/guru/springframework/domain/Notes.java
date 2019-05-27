045a850f87fd6af62d5b7ea5d26bb9ce0cfdc47e
Ferenc Bondar <fhery021@gmail.com>
2019-05-27 16:20:57 +0300

    MongoDB working on localhost

--- src/main/java/guru/springframework/domain/Notes.java
+++ src/main/java/guru/springframework/domain/Notes.java
@@ -2,6 +2,9 @@ package guru.springframework.domain;
 
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.DBRef;
+import org.springframework.data.mongodb.core.mapping.Document;
 
 
 /**
@@ -11,7 +14,9 @@ import lombok.Setter;
 @Setter
 public class Notes {
 
+    @Id
     private String id;
+    @DBRef
     private Recipe recipe;
     private String recipeNotes;
 
